932e667bc482c7d10b6718c71a64a685fc279805
tdegenko <tdegenko@u.rochester.edu>
2012-12-12 00:22:36 -0500

    Added logging and extended funtionality, and some testing code.

--- rocs/HTTPServ.rb
+++ rocs/HTTPServ.rb
@@ -1,11 +1,24 @@
+require 'open3'
 class HttpServer
-    #to be removed or changed once we have a config file reader
-    cgi_bin_path="cgi_bin/"
+    attr_accessor :host
+    attr_accessor :port
+    attr_accessor :cgi_bin_path
+    cgi_bin_path="cgi_bin"
     def process( request )
         # invoke the script in cgi_bin_path
         # pass in the parameters as a hashtable
         # return the results of the script
-        return %x(#{request.path} #{request.args})
+        stdin,stdout,stderr,wait_thr=Open3.popen3("ruby #{cgi_bin_path}/#{request.path} #{request.args}")
+        stdin.close
+        wait_thr.join
+        response=stdout.read
+        error=stderr.read
+        if error!=""
+            @log.error(error)
+            return error
+        else
+            return response
+        end
         # passing parameters as a hastable makes 
         # no sense, but it is what the specs say 
         #to do.
@@ -20,17 +33,45 @@ class HttpServer
         tcp = TcpServer.new( self )
         tcp.start
     end
-    def new
+    def initialize
         # is it really this simple?
         # it seems like it shouldnt be but is.
         yield self
+        @log=Logger.new()
     end
+end
+
+class Logger
+    def warn(str)
+    end
+    def info(str)
+    end
+    def error(str)
+        print "we have an error\n"
+    end
+end
+class TcpServer
+    def initialize (httpserv)
+    end
+    def start
+    end
+end
+
+class Request
+    attr_accessor :path
+    attr_accessor :args
 end
 
 server = HttpServer.new { |s|
   s.host = "localhost"
   s.port = 8000
-  s.cgi_bin_path = "galaxy/faraway"
+  s.cgi_bin_path = "cgi_bin"
 }
 
+req=Request.new()
+req.path="test.rb"
+req.args=Hash.new()
+
 server.start
+print server.process(req)
+
