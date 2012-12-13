5e7f5a4e3426a834541729eb3b2113485ddfc362
Sean Brennan <whalesonstilts2012@gmail.com>
2012-12-13 13:46:39 -0500

    pass the baton to simon

--- rocs/HttpRequest/HttpRequest.rb
+++ rocs/HttpRequest/HttpRequest.rb
@@ -7,15 +7,16 @@ class HttpRequest
 
     def initialize(request_text)
         # parse URI and its query string
-        uri = URI(URI.unescape(request_text))
+        uri = URI(request_text)
         @path = uri.path[/\/.*/] || "/"
-        @args = uri.query.nil? ? {} : parse_args(uri.query)
+        @args = uri.query.nil? ? {} : parse_args(URI.decode(uri.query))
     end
 
     def method_missing(method, *args, &block)
         name = method.to_s
         # handle regular name and safe name
         param = name =~ /^arg_/ ? name[4...name.size] : name
+        param.gsub!(/_/, " ")
         if @args.key? param then
             return @args[param]
         else
@@ -37,6 +38,7 @@ class HttpRequest
             if key == '' or value == '' then
                 raise ArgumentError, "query string contained invalid mapping: \"#{key}=#{value}\""
             end
+            key.gsub!(/\s/, " ")
             args[key] = value
         end
         return args
